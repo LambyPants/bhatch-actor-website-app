7c39161c2fcd8f6fdcff42645dd62650783b744c
Ryan Lambert <rllambert@rollins.edu>
2017-05-23 14:38:49 +0000

    add youtube links in navbar

---
+++ public/scripts/popupvideo.js
@@ -0,0 +1,25 @@
+var url = "";
+
+ console.log("its workking");
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+
+    console.log(url);
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal").on('hide.bs.modal', function(){
+        $("#cartoonVideo").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#myModal").on('show.bs.modal', function(){
+        $("#cartoonVideo").attr('src', url);
+    });
+
+
+//click listener which assigns url
+$('.demo-reels').on('click', 'a', (function(){
+ console.log($(this).attr('target'));
+ url = ($(this).attr('target'));
+}));
